b8e4a260700c0bfb9d4ef10549dd2d5403bfa831
nadapop0va <113141001+nadapop0va@users.noreply.github.com>
2022-09-12 21:22:11 +0400

    Практика1

---
+++ main.py
@@ -0,0 +1,12 @@
+#exercise 1
+def make_tshirt(size, text):
+    s = size +" "+ text
+    return s
+print(make_tshirt('L',"Python"))
+
+#exercise 2
+def make_tshirt2(size ="L",text ="I love python"):
+    s = size+" "+ text
+    return s
+print(make_tshirt2())
+print(make_tshirt2(size ="XS", text ="python"))
